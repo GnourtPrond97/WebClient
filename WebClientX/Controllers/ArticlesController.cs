c674d0118bad72f61cf9e12988575cd567535f94
GnourtPrond97 <truonghhth1905001@gmail.com>
2021-01-15 05:01:58 +0700

    Add project files.

---
+++ WebClientX/Controllers/ArticlesController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebClientX.Data;
+using WebClientX.Models;
+
+namespace WebClientX.Controllers
+{
+    public class ArticlesController : Controller
+    {
+        private WebClientXContext db = new WebClientXContext();
+
+        // GET: Articles
+        public ActionResult Index()
+        {
+            return View(db.Articles.ToList());
+        }
+
+        // GET: Articles/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Article article = db.Articles.Find(id);
+            if (article == null)
+            {
+                return HttpNotFound();
+            }
+            return View(article);
+        }
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebClientX/Models/Article.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebClientX.Models
+{
+    public class Article
+    {
+        public int Id { get; set; }
+        public string Link { get; set; }
+        public string Title { get; set; }
+        public string Images { get; set; }
+        public string Content { get; set; }
+
+        public Article()
+        {
+        }
+    }
+}
